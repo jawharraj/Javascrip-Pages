ae653daf57faff5d01b4f861953a11d2dfce1fac
jawharraj <137720725+jawharraj@users.noreply.github.com>
2023-08-01 21:30:20 +0530

    Project Completed

---
+++ Basic.js
@@ -0,0 +1,10 @@
+function func1() {
+    document.body.style.backgroundColor = "red";
+
+}
+function func2() {
+    document.body.style.backgroundColor = "green";
+}
+function func3() {
+    document.body.style.backgroundColor = "yellow";
+}
